3aaba1d82d42cbeda0382a25da4885ac1ffea889
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-15 16:50:33 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,10 +5,11 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  ""
+  "SELECT MIN(col_name) FROM table_name"
 end
 
 def average_student_gpa
+  
 end
 
 def total_tardies_for_all_students
