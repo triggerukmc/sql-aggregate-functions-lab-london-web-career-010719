370e67d22003b4af2e604ac55eca443c13ce6b72
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-15 16:51:26 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,11 +9,13 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  
+  "SELECT AVG(col_name) FROM table_name"
 end
 
 def total_tardies_for_all_students
+  
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(col_name) FROM table_name WHERE col_name = value"
 end
